042535813f8738e25cc80a28bc5d8691dcea1eba
Luis <luis2@example.com>
2026-04-11 01:39:50 +0200

    fix(wear): remove deprecated namespace in manifest and add play-services tasks coroutines for await

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,4 +41,6 @@ dependencies {
     implementation("androidx.activity:activity-compose:1.8.1")
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.compose.ui:ui-tooling-preview:1.5.4")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.7.3")
 }
+
