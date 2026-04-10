e5a50672bc00f56a7c85e0527e53d15a4f675a92
Luis <luis2@example.com>
2026-04-11 01:21:04 +0200

    fix: remove allprojects repositories to favor settings-level resolution

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,6 @@
-plugins {
+﻿plugins {
     id("com.android.application") version "8.11.1" apply false
     id("org.jetbrains.kotlin.android") version "2.2.20" apply false
 }
 
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
+// Repositories are managed in settings.gradle.kts to avoid conflicts with FAIL_ON_PROJECT_REPOS
