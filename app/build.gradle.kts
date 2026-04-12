6eb4246e291ed1deaae8831420ddcad6b5b54af8
morterix01 <morterix01@users.noreply.github.com>
2026-04-13 00:56:57 +0200

    Bump Wear OS version to 1.0.1-beta

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.luis.ducky_android"
         minSdk = 28
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.0.1"
     }
 
 
